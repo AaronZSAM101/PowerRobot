6211da817d768f5db9cd189872ad7853bd259773
Aaron_ZSAM <CaptainAaron101@gmail.com>
2024-10-19 11:46:36 +0800

    bugfix(PushMsg): 修复了电费足额但金额不足的仍旧推送消息的错误
    删除重复函数，调整推送文本内容

--- Notification.py
+++ Notification.py
@@ -7,12 +7,14 @@ from email.mime.multipart import MIMEMultipart
 # 推送电量余额通知
 def push_power_notification(config, remaining_balance, remaining_power, total_power):
     message_content = f"购买剩余电量: {remaining_power} 度\n总电量: {total_power} 度\n账户剩余金额: {remaining_balance:.2f} 元"
-    if remaining_balance <= 25:
+    
+    if remaining_balance <= config['remaining_balance_config'] and remaining_power <= config['remaining_power_config']:
+        print(f"剩余金额为 {remaining_balance:.2f} 元，再不充钱就等着断电吧。")
         message_content += "\n请及时续费！"
-        send_wechat_message(config['wx_robot_url'], message_content)
-        send_email(config, message_content)
+        send_wechat_message(config['wx_robot_url'], message_content, config)
+        send_email(config, message_content, subject="电沸桂消息提醒")
     else:
-        print(f"剩余金额为 {remaining_balance:.2f} 元，足够使用，无需续费。")
+        print(f"剩余金额为 {remaining_balance:.2f} 元，剩余电量为 {remaining_power} 度，足够使用，无需续费。")
 
 
 # 发送企业微信消息
@@ -36,13 +38,9 @@ def send_wechat_message(wx_robot_url, message_content, config):
     if response.status_code == 200:
         print("企业微信消息发送成功")
     else:
-        # 构建失败消息内容
         error_message = f"企业微信消息发送失败，状态码: {response.status_code}"
         print(error_message)
-        
-        # 调整报错信息发送逻辑，发送邮件通知
         send_email(config, error_message, subject="企业微信消息推送失败通知")
-
 
 
 # 发送邮件
@@ -56,36 +54,16 @@ def send_email(config, message_content, subject="电沸桂消息提醒"):
     msg.attach(MIMEText(message_content, 'plain'))
 
     try:
-        # 根据配置选择是否使用 SSL
         if config['use_ssl']:
             server = smtplib.SMTP_SSL(config['smtp_server'], config['smtp_port'])
         else:
             server = smtplib.SMTP(config['smtp_server'], config['smtp_port'])
-            server.starttls()  # 非SSL需要启动TLS
+            server.starttls()
 
         server.login(config['smtp_user'], config['smtp_password'])
-        # 调整报错信息发送方式
         server.sendmail(config['smtp_user'], config['recipient_email'] + config['admin_email'], msg.as_string())
         server.quit()
         print("邮件发送成功")
     except Exception as e:
         print(f"邮件发送失败: {e}")
-        # 发送企业微信消息通知邮件发送失败
         send_wechat_message(config['wx_robot_url'], f"邮件发送失败: {e}")
-
-
-# 推送逻辑及文本
-def push_notification(config, remaining_balance, remaining_power, total_power):
-    message_content = f"您的电费小助手电沸桂提醒您:\n\n购买剩余电量: {remaining_power} 度\n总电量: {total_power} 度\n账户剩余金额: {remaining_balance:.2f} 元"
-    
-    if remaining_balance <= 25:
-        print(f"剩余金额为 {remaining_balance:.2f} 元，再不充钱就等着断电吧。")
-        message_content += "\n请及时续费！"
-        send_wechat_message(config['wx_robot_url'], message_content, config)
-        send_email(config, message_content)
-
-    else:
-        print(f"剩余金额为 {remaining_balance:.2f} 元，足够使用，无需续费。")
-        # message_content += "\n请享受您的美好时光！"
-        # send_wechat_message(config['wx_robot_url'], message_content, config)
-        # send_email(config, message_content)
